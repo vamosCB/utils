3ff7882915d1e4bdbb473328b1a6b8e6e8da6ee8
vamoscb <965090342@qq.com>
2019-10-27 23:34:22 +0800

    增加安全处理Go协程的方法

---
+++ routines.go
@@ -0,0 +1,20 @@
+package utils
+
+var panicHandler func(interface{})
+
+//SetPanicHandler 注册panic 处理函数
+func SetPanicHandler(handler func(interface{})) {
+	panicHandler = handler
+}
+
+//SaftyRun Goroutines 安全执行，增加defer处理panic
+func SaftyRun(method func() (interface{}, error)) (resp interface{}, err error) {
+	defer func() {
+		if err := recover(); err != nil {
+			if panicHandler != nil {
+				panicHandler(err)
+			}
+		}
+	}()
+	return method()
+}
---
+++ routines_test.go
@@ -0,0 +1,18 @@
+package utils
+
+import (
+	"fmt"
+	"testing"
+)
+
+func panicFunc() (interface{}, error) {
+	testData := []int{1, 2, 3}
+	return testData[len(testData)], nil
+}
+func handlePanic(err interface{}) {
+	fmt.Printf("panic is %+v\n", err)
+}
+func Test_SaftyRun(t *testing.T) {
+	SetPanicHandler(handlePanic)
+	SaftyRun(panicFunc)
+}
