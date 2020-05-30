95bd29925920c7a67dc68f3b6346b7c8c0b10494
vamoscb <965090342@qq.com>
2020-05-30 18:55:20 +0800

    优化结构

---
+++ list/single_linked_list_test.go
@@ -0,0 +1,34 @@
+package list
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_Empty(t *testing.T) {
+	list := &SingleLinkedList{}
+	assert.Equal(t, true, list.Empty())
+}
+
+func Test_Length(t *testing.T) {
+	list := &SingleLinkedList{
+		Head: &Node{
+			Data: 1,
+			Next: &Node{
+				Data: 2,
+			},
+		},
+	}
+	assert.Equal(t, 2, list.Length())
+}
+
+func Test_Append(t *testing.T) {
+	list := &SingleLinkedList{}
+	list.Append(1)
+	list.Append(2)
+	list.Append(3)
+	list.Append(4)
+	t.Log("aaaaa")
+	assert.Equal(t, 4, list.Length())
+}
